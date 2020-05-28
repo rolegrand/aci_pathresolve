36de4d68a54f7a441c84c3e7074ce68c77a00ee5
Romain Legrand <rolegrand@gmail.com>
2020-05-28 11:23:37 +0200

    Printing Ajust and Check Results

--- aci_pathresolve.py
+++ aci_pathresolve.py
@@ -9,13 +9,35 @@ import cobra.modelimpl.fv.attentitypathatt
 import cobra.modelimpl.fv.stpathatt
 import cobra.modelimpl.fv.extstpathatt
 import requests
-import random
 import time
 import argparse
 # import pandas as pd
 requests.packages.urllib3.disable_warnings()
 
 mydict = {}
+
+
+def read_ext_file():
+    ## Reading external file to test extrating options
+    f = open("aci_path_hpr.conf", "r")
+    # print(f.read())
+    for i in f:
+        port_list = []
+        tenant_name, epg_name, node_name, port_list = extractinfo(i, port_list)
+        dict_building(tenant_name, epg_name, node_name, port_list)
+
+
+def dict_building(tenant_name, epg_name, node_name, port_list):
+    if epg_name == 'none':
+        pass
+    else:
+        mydict[tenant_name + '-' + epg_name] = {'tenant_name': tenant_name,
+                                                'epg_name': epg_name,
+                                                'node_name': node_name,
+                                                'port_name': port_list}
+
+    return mydict
+
 
 def extractinfo(input, port_list):
     tenant_name = input.split('tn-', 1)[1].split('/')[0]
@@ -43,8 +65,7 @@ def DeploymentQuery(session, mydict, epg):
 
     for i in testquery.children:
         for j in i.children:
-            print(j.dn)
-            # tenant_name, epg_name, node_name, port_list = extractinfo(str(j.dn), port_list)
+            tenant_name, epg_name, node_name, port_list = extractinfo(str(j.dn), port_list)
 
     try:
         node_name
@@ -55,15 +76,7 @@ def DeploymentQuery(session, mydict, epg):
         epg_name = 'none'
         tenant_name = 'none'
 
-    if epg_name == 'none':
-        pass
-    else:
-        random_number = random.randrange(65535)
-        mydict[random_number] = {'tenant_name': tenant_name,
-                             'epg_name': epg_name,
-                             'node_name': node_name,
-                             'port_name': port_list}
-
+    dict_building(tenant_name, epg_name, node_name, port_list)
     return mydict
 
 def main():
@@ -74,27 +87,40 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tenant')
     parser.add_argument('-e', '--epg')
+    parser.add_argument('-d', '--debug')
+
     args = parser.parse_args()
 
     tenant_input_filter = args.tenant
     epg_input_filter = args.epg
+    debug_input_filter = args.debug
 
     epg_query_def = cobra.mit.request.ClassQuery('fvAEPg')
     # Bulding argparse filters
-    if tenant_input_filter and epg_input_filter is not None:
-        epg_query_def.propFilter = 'and(wcard(fvAEPg.dn, "{}")wcard(fvAEPg.dn,"{}"))'.format('tn-' + tenant_input_filter, 'epg-' + epg_input_filter)
-    elif tenant_input_filter is not None:
-        epg_query_def.propFilter = 'wcard(fvAEPg.dn, "{}")'.format('tn-' + tenant_input_filter)
-    elif epg_input_filter is not None:
-        epg_query_def.propFilter = 'wcard(fvAEPg.dn, "{}")'.format('epg-' + epg_input_filter)
-    epg_query = session.query(epg_query_def)
+    if debug_input_filter is not None:
+        read_ext_file()
+    else:
+        if tenant_input_filter and epg_input_filter is not None:
+            epg_query_def.propFilter = 'and(wcard(fvAEPg.dn, "{}")wcard(fvAEPg.dn,"{}"))'.format('tn-' + tenant_input_filter, 'epg-' + epg_input_filter)
+        elif tenant_input_filter is not None:
+            epg_query_def.propFilter = 'wcard(fvAEPg.dn, "{}")'.format('tn-' + tenant_input_filter)
+        elif epg_input_filter is not None:
+            epg_query_def.propFilter = 'wcard(fvAEPg.dn, "{}")'.format('epg-' + epg_input_filter)
+        epg_query = session.query(epg_query_def)
 
-    for epg in epg_query:
-        DeploymentQuery(session, mydict, epg.dn)
+        for epg in epg_query:
+            DeploymentQuery(session, mydict, epg.dn)
 
     # pd_mydict = pd.DataFrame.from_dict(mydict, orient='index')
     # pd_mydict.to_excel('results.xlsx')
-    print(mydict)
+    result_read(mydict)
+
+def result_read(mydict):
+    print('{:<48} {:<48} {:<48} {:<48}'.format('TENANT', 'EPG', 'NODE', 'PORT_LIST'))
+    for k, v in mydict.items():
+        tenant_name, epg_name, node_name, port_name = v['tenant_name'], v['epg_name'], v['node_name'], v['port_name']
+        print('{:<48} {:<48} {:<48} {:<48}'.format(tenant_name, epg_name, node_name, str(port_name)))
+
 
 if __name__ == "__main__":
     start_time = time.time()
